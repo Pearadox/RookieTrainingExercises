3e46231759f54cf407a158bc1dfeedb91e5de79c
Bryan Li <bryanyuezhouli@gmail.com>
2020-07-01 20:40:04 -0500

    Did printing exercise

--- src/main/java/javaonly/printing/Printing.java
+++ src/main/java/javaonly/printing/Printing.java
@@ -4,5 +4,6 @@ public class Printing {
 
   public static void print() {
     // You should print "Hello Java" to standard output (use println, not print)
+    System.out.println("Hello Java");
   }
 }
