ac088826e5c0e28dc6ecb19fc4a93b8641a070d2
Bryan Li <bryanyuezhouli@gmail.com>
2020-09-18 18:42:54 -0500

    Fix FizzBuzz wording

--- src/main/java/javaonly/fizzbuzzconditional/FizzBuzzConditional.java
+++ src/main/java/javaonly/fizzbuzzconditional/FizzBuzzConditional.java
@@ -8,7 +8,7 @@ public class FizzBuzzConditional {
      * FizzBuzz is a classic programming interview question designed to filter out programmers that can't write an
      * if statement. This is the first of many variations that you will see in these exercises
      *
-     * If n is divisible by 3, then return "Fizz". If n is divisible by 5, then return "FizzBuzz"
+     * If n is divisible by 3, then return "Fizz". If n is divisible by 5, then return "Buzz"
      * If n is divisible by both 3 and 5, then return "FizzBuzz". If n isn't divisible by 3 or 5, return n as a
      * String.
      */
